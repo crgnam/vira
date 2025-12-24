750ccbe01b7540c953d7ea7e2132d2cccc074ef0
Chris Gnam <chrisrsg@gmail.com>
2025-12-23 20:45:38 -0500

    Fix download_tycho.bat syntax

--- scripts/download_tycho.bat
+++ scripts/download_tycho.bat
@@ -9,6 +9,7 @@ if "%~1"=="" (
     exit /b 1
 )
 
+set "BASE_DIR=%~1"
 if "%BASE_DIR:~-1%"=="\" set "BASE_DIR=%BASE_DIR:~0,-1%"
 
 set "OUTPUT_DIR=%BASE_DIR%\tycho2"
@@ -19,7 +20,7 @@ REM List of files
 set files=tyc2.dat.00 tyc2.dat.01 tyc2.dat.02 tyc2.dat.03 tyc2.dat.04^
  tyc2.dat.05 tyc2.dat.06 tyc2.dat.07 tyc2.dat.08 tyc2.dat.09 tyc2.dat.10^
  tyc2.dat.11 tyc2.dat.12 tyc2.dat.13 tyc2.dat.14 tyc2.dat.15 tyc2.dat.16^
- tyc2.dat.17 tyc2.dat.18 tyc2.dat.19 & REM suppl_1.dat suppl_2.dat
+ tyc2.dat.17 tyc2.dat.18 tyc2.dat.19
 
 REM Base URL
 set base_url=https://cdsarc.cds.unistra.fr/viz-bin/nph-Cat/txt?I/259
