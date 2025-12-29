4ee669060d3102b383466e7a76f25a5075bf83cf
Chris Gnam <chrisrsg@gmail.com>
2025-12-29 15:34:51 -0500

    Update conda recipe meta.yaml
    Fix custom cmake configuration to not overwrite include paths.
    
    Update the install paths of cmake helpers.

--- cmake/setup/CustomCompilerFlags.cmake
+++ cmake/setup/CustomCompilerFlags.cmake
@@ -3,32 +3,28 @@
 ##################################
 if(MSVC)
     set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
-
     # If on Windows:
     message("Windows Detected")
     set(CMAKE_INTERPROCEDURAL_OPTIMIZATION_Release ON)
     set(CMAKE_INTERPROCEDURAL_OPTIMIZATION_Debug OFF)
-
     add_compile_options(/utf-8) # Required for OpenImageIO
-
 elseif(UNIX AND NOT APPLE)
     # If on Linux:
     message("NIX Detected")
-
     # Check for the build type:
     if (NOT (CMAKE_BUILD_TYPE STREQUAL "Release"))
         set(CMAKE_BUILD_TYPE "Debug")
     endif()
     
-	if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-		set(CMAKE_CXX_FLAGS_DEBUG "-g -O0")
-
-	elseif(CMAKE_BUILD_TYPE STREQUAL "Release")
+    if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+        # Append to existing flags instead of overwriting
+        set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -g -O0")
+    elseif(CMAKE_BUILD_TYPE STREQUAL "Release")
         set(CMAKE_INTERPROCEDURAL_OPTIMIZATION ON)
-    	set(CMAKE_CXX_FLAGS "-Wall -mtune=native -march=native")
-		set(CMAKE_CXX_FLAGS_RELEASE "-O3 -fno-math-errno -fno-signed-zeros -fno-trapping-math -freciprocal-math -fno-rounding-math -fno-signaling-nans -fexcess-precision=fast -flto=auto")
-	endif()
-
+        # Append to existing flags instead of overwriting
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -mtune=native -march=native")
+        set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3 -fno-math-errno -fno-signed-zeros -fno-trapping-math -freciprocal-math -fno-rounding-math -fno-signaling-nans -fexcess-precision=fast -flto=auto")
+    endif()
     execute_process(
         COMMAND grep -i microsoft /proc/version
         RESULT_VARIABLE IS_WSL
@@ -39,11 +35,9 @@ elseif(UNIX AND NOT APPLE)
         message("WSL Detected")
         add_definitions(-D__WSL__)
     endif()
-
 elseif(APPLE)
     # If on Apple:
     message("Apple Detected")
-
     # Check if the linker supports -no_warn_duplicate_libraries
     include(CheckLinkerFlag)
     check_linker_flag(CXX "-Wl,-no_warn_duplicate_libraries" LINKER_SUPPORTS_NO_WARN_DUPLICATE_LIBRARIES)
