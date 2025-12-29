4bc4ead76fc126496a47dfcbdfeda950c7508ffa
Chris Gnam <chrisrsg@gmail.com>
2025-12-29 15:05:44 -0500

    Fix STB GIT_TAG

--- cmake/fetch/ViraFetchSTB.cmake
+++ cmake/fetch/ViraFetchSTB.cmake
@@ -24,7 +24,7 @@ if(NOT stb_FOUND)
     FetchContent_Declare(
         stb
         GIT_REPOSITORY https://github.com/nothings/stb.git
-        GIT_TAG f58f558c120e9b32c217290b80bad1a0729fbb2c  # Current main as of Aug 2025
+        GIT_TAG f1c79c02822848a9bed4315b12c8c8f3761e1296  # Current main as of Dec 2025
         GIT_SHALLOW ON
     )
     FetchContent_GetProperties(stb)
